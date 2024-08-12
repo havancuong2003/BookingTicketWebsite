e8e11f53b70e4863a29ade000efaad9229efd1fd
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-08-12 21:52:35 +0700

    merge: merge develop

--- src/pages/common/signup/signup.tsx
+++ src/pages/common/signup/signup.tsx
@@ -3,8 +3,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -13,23 +12,23 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props: any) {
-    return (
-        <Typography
-            variant="body2"
-            color="text.secondary"
-            align="center"
-            {...props}
-        >
-            {"Movie © "}
-            <Link color="inherit" href="https://mui.com/">
-                Bate
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
+// function Copyright(props: any) {
+//     return (
+//         <Typography
+//             variant="body2"
+//             color="text.secondary"
+//             align="center"
+//             {...props}
+//         >
+//             {"Movie © "}
+//             <Link color="inherit" href="https://mui.com/">
+//                 Bate
+//             </Link>{" "}
+//             {new Date().getFullYear()}
+//             {"."}
+//         </Typography>
+//     );
+// }
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
