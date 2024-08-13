c7d3810349d6058df51e0551ff6bb7dbb1638031
havancuong2003 <havancuong07122003@gmail.com>
2024-08-14 01:40:58 +0700

    fix: fix contact footer

--- src/components/commons/footer/footer.tsx
+++ src/components/commons/footer/footer.tsx
@@ -60,7 +60,7 @@ export function Footer() {
                                 <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                                 <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                             </svg>
-                            hacuong0712.work@gmail.com
+                            hacuong7123.work@gmail.com
                         </p>
                         <p className="mb-4 flex items-center justify-center md:justify-start">
                             <svg
