6a16bf4fb5a8672ea8c2f369d1fd69feeebcd9ef
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-08-23 23:08:19 +0700

    add lib

--- src/pages/admin/dashboard/dashboard.tsx
+++ src/pages/admin/dashboard/dashboard.tsx
@@ -1,117 +1,71 @@
-import { BarChart } from "@mui/x-charts";
+import React from "react";
+import { Chart } from "react-google-charts";
 
-export const dataset = [
-    {
-        london: 59,
-        paris: 57,
-        newYork: 86,
-        seoul: 21,
-        month: "Jan",
-    },
-    {
-        london: 50,
-        paris: 52,
-        newYork: 78,
-        seoul: 28,
-        month: "Feb",
-    },
-    {
-        london: 47,
-        paris: 53,
-        newYork: 106,
-        seoul: 41,
-        month: "Mar",
-    },
-    {
-        london: 54,
-        paris: 56,
-        newYork: 92,
-        seoul: 73,
-        month: "Apr",
-    },
-    {
-        london: 57,
-        paris: 69,
-        newYork: 92,
-        seoul: 99,
-        month: "May",
-    },
-    {
-        london: 60,
-        paris: 63,
-        newYork: 103,
-        seoul: 144,
-        month: "June",
-    },
-    {
-        london: 59,
-        paris: 60,
-        newYork: 105,
-        seoul: 319,
-        month: "July",
-    },
-    {
-        london: 65,
-        paris: 60,
-        newYork: 106,
-        seoul: 249,
-        month: "Aug",
-    },
-    {
-        london: 51,
-        paris: 51,
-        newYork: 95,
-        seoul: 131,
-        month: "Sept",
-    },
-    {
-        london: 60,
-        paris: 65,
-        newYork: 97,
-        seoul: 55,
-        month: "Oct",
-    },
-    {
-        london: 67,
-        paris: 64,
-        newYork: 76,
-        seoul: 48,
-        month: "Nov",
-    },
-    {
-        london: 61,
-        paris: 70,
-        newYork: 103,
-        seoul: 25,
-        month: "Dec",
-    },
+export const data = [
+    ["Element", "Lượt xem", { role: "style" }],
+    ["Tháng 1", 110, "#b87333"],
+    ["Tháng 2", 200, "silver"],
+    ["Tháng 3", 150, "gold"],
+    ["Tháng 4", 160, "blue"],
+    ["Tháng 5", 290, "red"],
+    ["Tháng 6", 400, "silver"],
+    ["Tháng 7", 500, "purple"],
+    ["Tháng 8", 321, "grey"],
+    ["Tháng 9", 420, "gold"],
+    ["Tháng 10", 311, "green"],
+    ["Tháng 11", 200, "pink"],
+    ["Tháng 12", 200, "brown"],
 ];
-
-const chartSetting = {
-    xAxis: [
-        {
-            label: "rainfall (mm)",
+export const options = {
+    chart: {
+        title: "Thống kê lượt xem phim đang chiếu",
+    },
+    hAxis: {
+        title: "Total Population",
+        minValue: 0,
+    },
+    vAxis: {
+        title: "City",
+    },
+    bars: "horizontal",
+    axes: {
+        y: {
+            0: { side: "right" },
         },
-    ],
-    width: 500,
-    height: 400,
+    },
 };
-
-const valueFormatter = (value: number | null) => `${value}mm`;
+export const data2 = [
+    ["Phim", "Ghế Thường", "Ghế Vip"],
+    ["Dragon Ball Super Heroes", 81750, 80080],
+    ["Batman", 37920, 36940],
+    ["Spiderman", 26950, 28960],
+    ["Kẻ cắp mặt trời", 20990, 19000],
+    ["Doraemon", 15260, 1500],
+];
 
 export function Dashboard() {
     return (
-        <BarChart
-            xAxis={[
-                { scaleType: "band", data: ["group A", "group B", "group C"] },
-            ]}
-            series={[
-                { data: [4, 3, 5] },
-                { data: [1, 6, 3] },
-                { data: [2, 5, 6] },
-            ]}
-            width={500}
-            height={300}
-        />
+        <>
+            <div>
+                <div style={{ display: "flex" }}>
+                    <Chart
+                        chartType="Bar"
+                        width="60%"
+                        height="400px"
+                        data={data2}
+                        options={options}
+                    />
+                </div>
+                <div style={{ display: "flex" }}>
+                    <Chart
+                        chartType="ColumnChart"
+                        width="60%"
+                        height="400px"
+                        data={data2}
+                    />
+                    <div>Thống kê lượt xem từng tháng</div>
+                </div>
+            </div>
+        </>
     );
 }
