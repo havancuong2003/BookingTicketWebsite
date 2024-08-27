943e93535df15a47f9dbee9ee76d6f53a184f9ed
havancuong2003 <havancuong07122003@gmail.com>
2024-08-27 16:12:19 +0700

    remove: remove unused imported

--- src/pages/admin/movie-control/add-new-movie/add-new-movie.tsx
+++ src/pages/admin/movie-control/add-new-movie/add-new-movie.tsx
@@ -8,7 +8,6 @@ import {
     FormControl,
 } from "@mui/material";
 import { useForm } from "react-hook-form";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { createMovie, getIDMovieAfterUpload } from "../../../../services";
 
--- src/services/admin/movie/movie.ts
+++ src/services/admin/movie/movie.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 // const navigate = useNavigate();
 export const getIDMovieAfterUpload = async (
     accessToken: string,
